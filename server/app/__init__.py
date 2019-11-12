e359c723dc3b5d68d97a05b8714c80bceabcb117
agurimon <agurimon18@gmail.com>
2019-11-12 16:33:43 +0900

    server init

---
+++ server/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+from config import CONNECT_STRING
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = CONNECT_STRING
+app.config['SECRET_KEY'] = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+db = SQLAlchemy(app)
+
+from app import models
+db.create_all()
+
+from app import routes
+
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ server/install.sh
@@ -0,0 +1,16 @@
+sudo apt install python3 python3-pip
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo apt install virtualenv
+virtualenv venv -p python3
+source ./venv/bin/activate
+pip install -r requirements.txt
+
+sudo apt install libmysqlclient-dev build-essential libssl-dev libffi-dev
+sudo apt install mysql-server
+
+sudo apt install nodejs npm
+ln -s /usr/bin/nodejs /usr/local/bin/node
+sudo npm install -g bower
+bower install
