e4f947cef5a1647697d0c9a3beb6ea383e5cc82f
agurimon <agurimon18@gmail.com>
2019-11-20 03:46:57 +0900

    선풍기 기본적인 동작, 타이머

--- server/app/routes/fan.py
+++ server/app/routes/fan.py
@@ -4,7 +4,15 @@ import time
 # from app.models.fan import Fan
 from app import app, db
 from app.models.fan import Fan
+#from motor import fan_speed, head_spin
+#from sensor import measure
 
+slow = 50
+middle = 70
+strong = 100
+
+# fan.speed()
+# head_spin(left, right, speed)
 def choose_mode(mode):
     fan = Fan.query.filter_by(id=1).one()
     fan.mode = mode
@@ -13,8 +21,49 @@ def choose_mode(mode):
     db.session.commit()
 
 
-# fan.speed()
-# head_spin(left, right, speed)
+@app.route('/wind/<int:value>', methods=["POST"])
+def wind_1(value):
+    # fan_speed(value)
+    print(value)
+    fan = Fan.query.filter_by(id=1).one()
+    fan.state = value
+    db.session.add(fan)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/spin', methods=["POST"])
+def spin():
+    # head_spin(10)
+
+    fan = Fan.query.filter_by(id=1).one()
+
+    if fan.spin_speed is 0:
+        fan.spin_speed = 10
+    else:
+        fan.spin_speed = 0
+
+    db.session.add(fan)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/timer', methods=["POST"])
+def timer():
+    time = request.form['time']
+    fan = Fan.query.filter_by(id=1).one()
+
+    current_time = time.time()
+
+
+    if current_time+1000:
+        fan.speed = 0
+
+
+    return redirect(url_for('index'))
+
 
 # 노말 모드 1
 @app.route('/normal', methods=["GET", "POST"])
